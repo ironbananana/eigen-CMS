fab63effeaeb6493a312a3287129d56fcf852728
Quinten <90308786+QNoah@users.noreply.github.com>
2022-02-02 21:33:14 +0100

    Commit 02/02/2022
    Headers zijn in apparte php, nieuwe page toegevoegt, alle styling is nu in 1 file
    
    Co-Authored-By: CcharlotteHh <90309917+CcharlotteHh@users.noreply.github.com>
    Co-Authored-By: Nick v Z. <70808033+NickvZ34@users.noreply.github.com>
    Co-Authored-By: Thomas van Steen  <90308865+ironbananana@users.noreply.github.com>
    Co-Authored-By: CcharlotteHh <90309917+CcharlotteHh@users.noreply.github.com>
    Co-Authored-By: Nick v Z. <70808033+NickvZ34@users.noreply.github.com>
    Co-Authored-By: Thomas van Steen  <90308865+ironbananana@users.noreply.github.com>

---
+++ CMS/berichten.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Eigen Cms</title>
+</head>
+<body class="bodyBerichten">
+      <?php include('header.php'); ?>
+
+      <h1>Berichten</h1>
+      <div class="berichten">
+          <div class="nummerBericht">0- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+          <hr>
+          <div class="nummerBericht">1- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+          <hr>
+          <div class="nummerBericht">2- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+          <hr>
+          <div class="nummerBericht">3- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+          <hr>
+          <div class="nummerBericht">4- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+          <hr>
+          <div class="nummerBericht">5- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+          <hr>
+          <div class="nummerBericht">6- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+          <hr>
+          <div class="nummerBericht">7- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+      </div>
+      <div class="paginas">
+      <div class="vorigePagina">Vorige pagina</div>
+      <div class="volgendePagina">Volgende Pagina</div>
+    </div>
+
+
+
+      <!-- footer -->
+
+<div class="footer"></div>
+
+</body>
+</html>
