0546c04a43c82f68e19472764f625d4fb7551029
CcharlotteHh <90309917+CcharlotteHh@users.noreply.github.com>
2022-02-04 15:00:38 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
 </head>
-<body>
-<form method="POST" action="registratie.php">
-    <input type="submit"/>
-  </form>
-  <form method="POST" action="inlog.php">
-    <input type="submit"/>
-  </form>
+
+<body class="split-container">
+
+    <div class="box1">
+        <!--box 1 contents-->
+    </div>
+
+    <div class="box2">
+        <!--box 2 contents-->
+        <div class="button_box">
+        <form method="POST" action="registratie.php">
+        <input type="submit" value="Register" class="ibutton"/>
+        </form>
+        <form method="POST" action="inlog.php">
+            <input type="submit" value="Log in" class="ibutton"/>
+        </form>
+        </div>
+        
+    </div>
+
 </body>
+
 </html>
