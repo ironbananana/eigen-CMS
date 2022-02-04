c553e079258abeea9b72beb4c08eb79f600da9fa
CcharlotteHh <90309917+CcharlotteHh@users.noreply.github.com>
2022-02-04 11:53:58 +0100

    Add files via upload

--- CMS/index.php
+++ CMS/index.php
@@ -4,43 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CMS Login/Registration</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+    <title>Document</title>
 </head>
-<body id="index">
-    <div class="loginbox">
-        <div id="loginText"><b>Login</b></div>
-        <div class="loginInputs">
-        <input type="text" name="gebruikerLogin" required placeholder="Gebruikers Naam">
-        <input type="password" name="wachtwoordLogin" required placeholder="Wachtwoord">
-        </div>
-        <input type="submit" value="Login" id="loginKnop">     
-    </div>
-
-    <div class="registerbox">
-        <div id="registerText"><b>Registeren</b></div>
-        <div class="registerInputs">
-          Voornaam  <input type="text" name="vnaam" required minlength="3">
-          <br>
-          <br>
-          Achternaam  <input type="text" name="anaam" required minlength="3">
-          <br>
-          <br>
-          Tel Nr  <input type="number" name="anaam" required maxlength="10">
-          <br>
-          <br>
-          Email  <input type="text" name="emailRegisteren" required>
-          <br>
-          <br>
-          Wachtwoord  <input type="password" name="wachtwoordRegisteren" required>
-          <br>
-          <br>
-        </div>
-        <input type="submit" value="Registeren" id="registerKnop">
-    </div>
-    </div>
+<body>
+<form method="POST" action="registratie.php">
+    <input type="submit"/>
+  </form>
+  <form method="POST" action="inlog.php">
+    <input type="submit"/>
+  </form>
 </body>
 </html>
---
+++ CMS/inlog.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS Login/Registration</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+</head>
+<body id="index">
+    <div class="loginbox">
+        <div id="loginText"><b>Login</b></div>
+        <div class="loginInputs">
+        <input type="text" name="gebruikerLogin" required placeholder="Gebruikers Naam">
+        <input type="password" name="wachtwoordLogin" required placeholder="Wachtwoord">
+        </div>
+        <input type="submit" value="Login" id="loginKnop">     
+    </div>
+    </div>
+</body>
+</html>
