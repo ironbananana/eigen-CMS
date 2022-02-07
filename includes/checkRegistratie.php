2e7b3f320005e40e8b8d132488759ea8d533bfdd
Nick v Z <70808033+NickvZ34@users.noreply.github.com>
2022-02-07 10:45:00 +0100

    Directory Updeet 2.0

--- includes/checkRegistratie.php
+++ includes/checkRegistratie.php
@@ -3,27 +3,27 @@
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
         if (trim($_POST['vnaam']) == NULL) {
-            header("Location: registratie?error=voornaamleeg");
+            header("Location: ../registratie?error=voornaamleeg");
         }
         
         if (trim($_POST['anaam']) == NULL) {
-            header("Location: registratie?error=voornaamleeg");
+            header("Location: ../registratie?error=voornaamleeg");
         }
 
         if (trim($_POST['telnummer']) == NULL) {
-            header("Location: registratie?error=telleeg");
+            header("Location: ../registratie?error=telleeg");
         }
 
         if (trim($_POST['emailadres']) == NULL) {
-            header("Location: registratie?error=emaileeg");
+            header("Location: ../registratie?error=emaileeg");
         }
     
         if (trim($_POST['wachtwoord']) == NULL) {
-            header("Location: registratie?error=wwleeg");
+            header("Location: ../?error=wwleeg");
         }
         
         if (trim($_POST['wwrepeat']) == NULL) {
-            header("Location: registratie?error=ww2leeg");
+            header("Location: ../registratie?error=ww2leeg");
         }
 
         require_once 'db_conn.php';
@@ -47,20 +47,20 @@
                 mysqli_query($conn, "INSERT INTO `users` (`Emailadres`, `Voornaam`, `Achternaam`, `Wachtwoord`, `Telnummer`) VALUES ('$emailadres', '$voornaam', '$achternaam', '$hasedWW', '$telnummer') ");
 
                 // Verstuur een email naar het emailadres met een uitnodiging / bevestiging.
-                header('Location: ./inlog?success=registratie');
+                header('Location: ../inlog?success=registratie');
                 exit();
 
             } else {
-                header('Location: ./registratie?error=pwdnomatch');
+                header('Location: ../registratie?error=pwdnomatch');
                 exit();
             }
 
         } else {
-            header('Location: ./registratie?error=doublemail');
+            header('Location: ../registratie?error=doublemail');
             exit();
         }
 
     } else {
-        header('Location: ./registratie?error=optyfengauw');
+        header('Location: ../registratie?error=optyfengauw');
         exit();
     }
