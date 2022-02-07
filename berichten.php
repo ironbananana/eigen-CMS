5aab40a8a3f654c254dbb5871c14da7a2fadab2d
CcharlotteHh <90309917+CcharlotteHh@users.noreply.github.com>
2022-02-07 14:56:37 +0100

    Add files via upload
    berichtenpagina aangepast zodat alles klopt met de styling

--- berichten.php
+++ berichten.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 
-  session_start();
 
-  if (!$_SESSION['loggedin']) {
-    header('Location: /inlog?error=nietingelogd');
-    exit();
-  }
+session_start();
 
-  $config['titel'] = "Berichten";
-  include_once 'includes/header.php';
+// if (!$_SESSION['loggedin']) {
+//header('Location: ./inlog?error=nietingelogd');
+// exit();
+//}
 
-?> 
+$config['titel'] = "Berichten";
+include_once 'includes/header.php';
+
+?>
 
 <body class="bodyBerichten">
   <div class="topnav">
-    <br>
-    <a class="navigation" href="#home">Home</a>
+    <a href="#home">Home</a>
     <a href="#About">About</a>
     <a href="#Toevoegen">Bericht +</a>
+    <a class="messagetitle" href="#backtofirstmessagepage">Messages</a>
+    
   </div>
-  <div class="messagetitle">
-    <h1>Berichten</h1>
-  </div>
+
   <div class="berichten">
     <div class="nummerBericht">0- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
     <hr>
@@ -38,12 +38,18 @@
     <div class="nummerBericht">6- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
     <hr>
     <div class="nummerBericht">7- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
-
-    <div class="pagebuttons">
-      <button class="vorigePagina">Vorige pagina</button>
-      <button class="volgendePagina">Volgende Pagina</button>
-    </div>
+    <hr>
+    <div class="nummerBericht">8- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+    <hr>
+    <div class="nummerBericht">9- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
+    <hr>
+    <div class="nummerBericht">10- Bericht | Quinten | 01-02-2022 | 19:30 | Verwijder bericht</div>
   </div>
+  <div class="pagebuttons">
+    <button class="ibutton">Vorige pagina</button>
+    <button class="ibutton">Volgende Pagina</button>
+  </div>
+
 
 
 
