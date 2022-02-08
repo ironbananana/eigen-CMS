8de90ed5f4dc3ba6d65093bee85a791156e1123f
CcharlotteHh <90309917+CcharlotteHh@users.noreply.github.com>
2022-02-08 09:46:19 +0100

    Add files via upload
    script toegevoegd voor password function

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
   <script src="https://kit.fontawesome.com/c3875a19f9.js" crossorigin="anonymous"></script>
+  <script src="assets/js/script.js"></script>
   
   <title><?php echo $config['titel']; ?> </title>
 </head>
